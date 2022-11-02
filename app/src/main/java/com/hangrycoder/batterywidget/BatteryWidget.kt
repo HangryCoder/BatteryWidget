f1d94d90f196ea9b09b77761d1b7ca258ad8732e
Sonia <soniawadji@gmail.com>
2022-11-02 14:11:28 +0530

    Added a new mobile icon

--- app/src/main/java/com/hangrycoder/batterywidget/BatteryWidget.kt
+++ app/src/main/java/com/hangrycoder/batterywidget/BatteryWidget.kt
@@ -5,11 +5,10 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
 
-/**
- * Implementation of App Widget functionality.
- */
 class BatteryWidget : AppWidgetProvider() {
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+    override fun onUpdate(
+        context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray
+    ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -25,11 +24,14 @@ class BatteryWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+internal fun updateAppWidget(
+    context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int
+) {
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.battery_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setTextViewText(R.id.battery_value, widgetText)
+    views.setImageViewResource(R.id.battery_icon, R.drawable.ic_phone)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
